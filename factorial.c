89ae5ebad9664e0537c4088e305972fe228368ab
shivendra9120 <25113cn277@glbitm.ac.in>
2026-03-20 15:13:02 +0530

    factorial of given number

---
+++ factorial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+long long factorial(int n){
+    if(n==0 , n ==1 )
+    return 1;
+    else 
+         return n * factorial(n-1);
+}
+     int main(){
+        int n;
+        printf("Enter a number:");
+        scanf("%d",&n);
+        if(n<0)
+        printf(" Factorial not defined for negative number");
+        else 
+         printf("Factorial of %d=%lld",n,factorial(n));
+          return 0;
+    }
