ff3b690aea0fcbcb6710e8ed0578e5754d99d749
shivendra9120 <25113cn277@glbitm.ac.in>
2026-03-20 13:55:17 +0530

    quadraticequation

---
+++ quadraticequation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+int a,b,c;
+printf9("input the values");
+scanf("%d",a);
+printf("Enter the values");
+scanf("%d\n",b);
+switch(                                                                                            )
+    }
+}
